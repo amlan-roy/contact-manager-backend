f4b1f6899921bad83bc252edd5edd0aa0da07efc
Amlan Roy <59330872+amlan-roy@users.noreply.github.com>
2024-10-31 13:51:37 +0530

    Add contacts controller and use it

---
+++ controllers/contact.js
@@ -0,0 +1,52 @@
+/**
+ * @description Get all contacts
+ * @route GET /api/contacts
+ * @access public
+ */
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+};
+
+/**
+ * @description Get a contact
+ * @route GET /api/contacts/:id
+ * @access public
+ */
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contact with id: ${req.params.id}` });
+};
+
+/**
+ * @description create a new contact
+ * @route POST /api/contacts
+ * @access public
+ */
+const createContact = (req, res) => {
+  res.status(201).json({ message: "Create contact" });
+};
+
+/**
+ * @description update a contact
+ * @route UPDATE /api/contacts/:id
+ * @access public
+ */
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update contact with id: ${req.params.id}` });
+};
+
+/**
+ * @description delete contact
+ * @route DELETE /api/contacts/:id
+ * @access public
+ */
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete contact with id: ${req.params.id}` });
+};
+
+module.exports = {
+  getContact,
+  getContacts,
+  createContact,
+  updateContact,
+  deleteContact,
+};
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-});
-router.get("/:id", (req, res) => {
-  res.status(200).json({ message: `Get contact with id: ${req.params.id}` });
-});
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Create contact" });
-});
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update contact with id: ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete contact with id: ${req.params.id}` });
-});
+const {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+} = require("./../controllers/contact");
+
+router.route("/").get(getContacts).post(createContact);
+
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
